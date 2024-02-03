6582c65e5c6b7528a4cbb400af7a527f045a246f
Vasiliy Zadoya <cool29@yandex.ru>
2024-02-03 18:56:58 +0500

    add vite enviroment: BASE_URL

--- src/api/const.ts
+++ src/api/const.ts
@@ -3,7 +3,7 @@ export const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
 };
 
-// export const BASE_URL = 'http://89.108.88.53:8080/api/v1';
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL =
+  import.meta.env.VITE_BASE_URL ?? 'http://89.108.88.53:8080/api/v1';
 
 export const LS_KEY_API_TOKEN = 'LS_KEY_API_TOKEN';
